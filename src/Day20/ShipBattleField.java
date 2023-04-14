a9d2e2831b3d2b929fe5d311ef0e9562d309cd1d
F-Jahura <f_jahura@yahoo.com>
2023-04-14 19:57:37 +0300

    Final work

---
+++ src/Day20/ShipBattleField.java
@@ -0,0 +1,85 @@
+package Day20;
+
+import java.util.InputMismatchException;
+import java.util.MissingFormatArgumentException;
+import java.util.Scanner;
+
+public class ShipBattleField {
+    public static void battleField(int[][] playerField1, int[][] playerField2, String playerName1, String playerName2){
+        int[][] battleField1 = new int[10][10];
+        int[][] battleField2 = new int[10][10];
+        String currentPlayerName = playerName1;
+        int[][] currentPlayerField = playerField2;
+        int[][] currentBattleField = battleField1;
+        Scanner scanner = new Scanner(System.in);
+
+            while (isPlayerAlive(playerField1) && isPlayerAlive(playerField2)) {
+                try {
+                    System.out.println();
+                    System.out.println(currentPlayerName + "'s battle field.");
+                    printField(currentBattleField);
+                    System.out.print(currentPlayerName + ", please enter xShot: ");
+                    int xShot = scanner.nextInt();
+                    System.out.print(currentPlayerName + ", please enter yShot: ");
+                    int yShot = scanner.nextInt();
+                    int shotFieldResult = shotField(currentPlayerField, currentBattleField, xShot, yShot);
+                    if (shotFieldResult == 0) {
+                        System.out.print("Please enter next player: 1. First player; 2. Second player - ");
+                        int nextPlayer = scanner.nextInt();
+                        if (nextPlayer == 1) {
+                            currentPlayerName = playerName1;
+                            currentPlayerField = playerField2;
+                            currentBattleField = battleField1;
+                        } else if (nextPlayer == 2) {
+                            currentPlayerName = playerName2;
+                            currentPlayerField = playerField1;
+                            currentBattleField = battleField2;
+                        }
+                    }
+                } catch (InputMismatchException e) {
+                    System.out.printf("%s is not an int%n", scanner.nextLine());
+                } catch (MissingFormatArgumentException e) {
+                    System.out.printf("%s is not an int%n", scanner.nextLine());
+                }
+            }
+        System.out.println(currentPlayerName + " is winner");
+
+    }
+
+    public static int shotField(int[][] playerField, int[][] battleField, int xShot, int yShot){
+        if (playerField[xShot][yShot] == '1'){
+            playerField[xShot][yShot] = '#';
+            battleField[xShot][yShot] = '#';
+            System.out.println("Good shot");
+            return 1;
+        }
+        System.out.println("Bad shot");
+        return 0;
+    }
+
+    public static boolean isPlayerAlive(int[][] playerField){
+        for (int[] cells: playerField){
+            for (int cell: cells){
+                if (cell == '1'){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static void printField(int[][] playerField){
+        for (int[] cells : playerField){
+            for (int cell : cells){
+                if (cell == 0){
+                    System.out.print("⬜");
+                }
+                else {
+                    System.out.print("\uD83D\uDD25");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+}
---
+++ src/Day20/ShipField.java
@@ -0,0 +1,108 @@
+package Day20;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class ShipField {
+    static Scanner scanner = new Scanner(System.in);
+    public static void displayShips(int[][] playerField) {
+        int x;
+        int y;
+        int position;
+        int shipSize;
+        for (int k = 0; k < 10; k++) {
+
+            while (true) {
+                try {
+                    System.out.print("Please enter ship size 1 to 4: ");
+                    shipSize = scanner.nextInt();
+                    if (shipSize >= 1 && shipSize <= 4) {
+                        break;
+                    }
+                    System.out.println("The number should be between 1 and 4. Please try again.");
+                }catch (InputMismatchException e){
+                    System.out.printf("%s is not an int%n", scanner.nextLine());
+                }
+            }
+
+            while (true) {
+                try {
+                    System.out.print("Please enter ship position: 1. Horizontal; 2. Vertical - ");
+                    position = scanner.nextInt();
+                    if (position == 1 || position == 2) {
+                        break;
+                    }
+                    System.out.println("The number should be 1 or 2. Please try again.");
+                } catch (InputMismatchException e) {
+                    System.out.printf("%s is not an int%n", scanner.nextLine());
+                }
+            }
+
+            while (true) {
+                try {
+                    System.out.print("Please enter x: ");
+                    x = scanner.nextInt();
+                    System.out.print("Please enter y: ");
+                    y = scanner.nextInt();
+                    int freeSpaceResult = freeSpace(x, y, position, shipSize, playerField);
+                    if (freeSpaceResult == 0) {
+                        break;
+                    }
+                    System.out.println("No free spaces. Please try again.");
+                } catch (ArrayIndexOutOfBoundsException e) {
+                    System.out.println("The number is out of bounds. Please enter again.");
+                }
+                catch (InputMismatchException e){
+                    System.out.printf("%s is not an int%n", scanner.nextLine());
+                }
+            }
+
+            if (position == 1){
+                for (int a = 0; a < shipSize; a++){
+                    playerField[x+a][y] = '1';
+                }
+            }
+            if (position == 2){
+                for (int b = 0; b < shipSize; b++){
+                    playerField[x][y+b] = '1';
+                }
+            }
+        }
+        printField(playerField);
+    }
+
+    public static int freeSpace(int x, int y, int position, int shipSize, int[][] playerField){
+        if (position == 1){
+            for (int i = 0; i < shipSize-1; i++){
+                if (playerField[x+i][y] == '1' || playerField[x+i+1][y] == '1' || playerField[x+i-1][y] == '1' ||
+                        playerField[x+i][y+1] == '1' || playerField[x+i][y-1] == '1' || (x+i) > 9 || (x+i) < 0){
+                    return -1;
+                }
+            }
+        }
+
+        if (position == 2){
+            for (int j = 0; j < shipSize-1; j++){
+                if (playerField[x][y+j] == '1' || playerField[x+1][y] == '1' || playerField[x-1][y] == '1' ||
+                        playerField[x][y+j+1] == '1' || playerField[x][y+j-1] == '1' || (y+j) > 9 || (y+j) < 0){
+                    return -1;
+                }
+            }
+        }
+        return 0;
+    }
+
+    public static void printField(int[][] playerField){
+        for (int[] cells : playerField){
+            for (int cell : cells){
+                if (cell == 0){
+                    System.out.print("⬜");
+                }
+                else {
+                    System.out.print("🛥");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
